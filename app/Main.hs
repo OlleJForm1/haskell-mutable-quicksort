5a0a460c77f161c3c6ae01e7565e895a07f3822d
Olle Jernelöf <olle@form1.se>
2024-06-28 16:10:13 +0200

    very clean version

--- app/Main.hs
+++ app/Main.hs
@@ -1,45 +1,36 @@
 module Main where
 
-import Control.Monad.ST (runST, ST)
-import qualified Data.Vector as V
+import Control.Monad.ST (ST)
 import qualified Data.Vector.Mutable as VM
-import Control.Monad (when)
+import Control.Monad (when, (>=>))
 import Control.Monad.Loops (untilJust, untilM_)
 import Data.STRef (newSTRef, modifySTRef, readSTRef)
 import Data.Functor (($>))
-import Data.Bifoldable (bimapM_)
+import Lib (withSTVector, bothM_, bothM)
+import Control.Monad.Fix (fix)
+import Data.Function ((&))
 
 quickSort :: Ord a => [a] -> [a]
-quickSort xs = runST $ do
-    let a = V.fromList xs
-    res <- V.thaw a
-    quickSort' res
-    V.toList <$> V.freeze res
+quickSort = withSTVector $ fix $ \rec v ->
+    when (VM.length v > 1) $ do
+        partition v >>= bothM_ rec
   where 
-    quickSort' :: Ord a => VM.STVector s a -> ST s ()
-    quickSort' a = when (VM.length a > 1) 
-                 $ partition a >>= bimapM_ quickSort' quickSort'
-
     partition :: Ord a => VM.STVector s a -> ST s (VM.STVector s a, VM.STVector s a)
     partition a = do
         p <- VM.read a 0
-        l <- newSTRef (-1)
-        h <- newSTRef $ VM.length a
+        (l, h) <- newSTRef `bothM` (-1, VM.length a)
         untilJust $ do
             increment l `untilM_` (p <=) <$> (a `at` l)
             decrement h `untilM_` (p >=) <$> (a `at` h)
-            l' <- readSTRef l
-            h' <- readSTRef h
+            (l', h') <- readSTRef `bothM` (l, h)
             if l' < h'
               then VM.swap a l' h' $> Nothing
-              else pure $ Just $ VM.splitAt (h' + 1) a
+              else VM.splitAt (h' + 1) a & Just & pure
 
-    increment r = modifySTRef r (+ 1)
-    decrement r = modifySTRef r (+ (-1))
-    at a r = VM.read a =<< readSTRef r
-
-
+    increment = (`modifySTRef` (+   1))
+    decrement = (`modifySTRef` (+ (-1)))
+    at a = readSTRef >=> VM.read a
 
 main :: IO ()
-main = print $ quickSort [1, 5, 3, 6, 7, 22, 4, 6, 2, 3, 44] 
+main = print $ quickSort ([1, 5, 3, 6, 7, 22, 4, 6, 2, 3, 44] :: [Int])
 
